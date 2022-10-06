8dabf927d31d928463091972a02aaae57a291d5a
manishasawale <manishasawale77@gmail.com>
2022-10-07 00:11:28 +0530

    tc_writecode
    commit to team member

---
+++ githunproject1/src/githunproject1/githubpractice.java
@@ -0,0 +1,11 @@
+package githunproject1;
+
+public class githubpractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" write github code");
+	}
+
+}
